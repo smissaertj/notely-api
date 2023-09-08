c04fed08d4f6ad80f43bccbd19dcdfd9fff3def9
Joeri Smissaert <smissaertj@gmail.com>
2023-09-08 21:24:25 +0400

    fix: ensure note is a named export

--- src/models/note.js
+++ src/models/note.js
@@ -17,6 +17,4 @@ const noteSchema = new mongoose.Schema(
   },
 );
 
-const Note = mongoose.model('Note', noteSchema);
-
-export default { Note };
+export const Note = mongoose.model('Note', noteSchema);
