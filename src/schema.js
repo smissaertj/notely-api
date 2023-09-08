1156ef0c0fc2c292ebe2638f0f92864cb7569870
Joeri Smissaert <smissaertj@gmail.com>
2023-09-08 21:25:00 +0400

    refactor: notes resolver reads from MongoDB

--- src/schema.js
+++ src/schema.js
@@ -1,28 +1,30 @@
-import { gql } from "graphql-tag"
+import { gql } from 'graphql-tag';
+import { Note } from './models/note.js';
+
 // Mock data
 let notes = [
-  {id: '1', content: 'Note 1', author: 'Joeri'},
-  {id: '2', content: 'Note 2', author: 'Dave'},
-  {id: '3', content: 'Note 3', author: 'Mike'},
-  {id: '4', content: 'Note 4', author: 'Steve'},
-]
+  { id: '1', content: 'Note 1', author: 'Joeri' },
+  { id: '2', content: 'Note 2', author: 'Dave' },
+  { id: '3', content: 'Note 3', author: 'Mike' },
+  { id: '4', content: 'Note 4', author: 'Steve' },
+];
 
 // Construct a schema using GraphQL Schema Language
 export const typeDefs = gql`
-  type Query{
+  type Query {
     hello: String
     "Fetch all Notes."
     notes: [Note!]!
     "Fetch a specific Note by ID."
     note(id: ID!): Note
   }
-  
-  type Mutation{
+
+  type Mutation {
     "Add a new Note."
     newNote(content: String!): Note!
   }
-  
-  type Note{
+
+  type Note {
     "The Note's ID."
     id: ID!
     "The Note content."
@@ -30,30 +32,34 @@ export const typeDefs = gql`
     "The Note author."
     author: String!
   }
-`
+`;
 
 // Provide resolver functions for our Schema fields
 export const resolvers = {
   Query: {
     // Return Hello World for testing purposes
-    hello() { return  'Hello World!'},
+    hello() {
+      return 'Hello World!';
+    },
     // Return an array of notes to populate the client applications
-    notes(){ return notes },
+    notes: async () => {
+      return await Note.find();
+    },
     // Return a specific note by ID
     note(parent, args, contextValue, info) {
-      return notes.find(note => note.id === args.id)
-    }
+      return notes.find((note) => note.id === args.id);
+    },
   },
   Mutation: {
     // Add a new note and returns the note
-    newNote(parent, args, contextValue, info){
+    newNote(parent, args, contextValue, info) {
       let noteValue = {
         id: String(notes.length + 1),
         content: args.content,
-        author: "Joeri"
-      }
-      notes.push(noteValue)
-      return noteValue
-    }
-  }
-}
+        author: 'Joeri',
+      };
+      notes.push(noteValue);
+      return noteValue;
+    },
+  },
+};
