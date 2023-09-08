5bfe1e750b47fe6ae1fe85311017409aa766f332
Joeri Smissaert <smissaertj@gmail.com>
2023-09-08 21:11:49 +0400

    refactor: added Note db model

---
+++ src/models/note.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const noteSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+  },
+  // Assigns createdAt and updatedAt fields with a Date type
+  {
+    timestamps: true,
+  },
+);
+
+const Note = mongoose.model('Note', noteSchema);
+
+export default { Note };
